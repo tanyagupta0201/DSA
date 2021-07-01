6e9f62ad473c8641cfb8bf3dc55ef4cf84c38d89
Tanya Gupta <tanyagupta.pg@gmail.com>
2021-07-01 18:26:30 +0530

    added new code

---
+++ Arrays/1-D Array/Ranks.cpp
@@ -0,0 +1,52 @@
+// Replace each element of the array with it's corresponding rank
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int compare(int n1, int n2)
+{
+    return n1 > n2;
+}
+
+void ranks(int a[], int n)
+{
+    map<int, int> m;
+
+    int b[n];
+    // Copy the elements of array a into array b
+    for (int i = 0; i < n; i++)
+    {
+        b[i] = a[i];
+    }
+
+    // Sort in descending order
+    sort(b, b + n, compare);
+
+    for (int index = 0; index < n; index++)
+    {
+        m.insert(make_pair(b[index], index + 1));
+    }
+
+    // Assigning rank to the elements
+    for (int index = 0; index < n; index++)
+    {
+        a[index] = m[a[index]];
+    }
+}
+
+int main()
+{
+    int a[] = {200, 50, 250, 100};
+    int n = sizeof(a) / sizeof(int);
+
+    ranks(a, n);
+
+    cout << "[";
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << a[i] << ", ";
+    }
+    cout << a[n - 1] << "]";
+
+    return 0;
+}
