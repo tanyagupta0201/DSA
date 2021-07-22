5edd9aaeb83cea21b330a643e1e2da0075f696c4
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2021-07-22 20:23:11 +0530

    Update SpiralPrint.cpp

--- Arrays/2-D Array/SpiralPrint.cpp
+++ Arrays/2-D Array/SpiralPrint.cpp
@@ -1,55 +1,67 @@
+/*
+    
+       Input:  1   2   3   4  5   6
+               7   8   9  10  11  12
+               13  14  15 16  17  18
+               
+       Output: 1 2 3 4 5 6 12 18 17 16 15 14 13 7 8 9 10 11
+       
+       Explanation :The output is matrix in spiral format.
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 void Spiral(int a[][20], int m, int n)
 {
-    int StartRow = 0, StartCol = 0, EndRow = m - 1, EndCol = n - 1;
+    int direction = 0;
+    int top = 0, bottom = m - 1, left = 0, right = n - 1;
 
-    while (StartRow <= EndRow && StartCol <= EndCol)
+    while (top <= bottom and left <= right)
     {
-        // First Row
-        for (int i = StartCol; i <= EndCol; i++)
+        if (direction == 0)
         {
-            cout << a[StartRow][i] << " ";
-        }
-        StartRow++;
-
-        // End Column
-        for (int i = StartRow; i <= EndCol; i++)
-        {
-            cout << a[i][EndCol] << " ";
-        }
-        EndCol--;
-
-        // Bottom Row
-        if (EndRow > StartRow)
-        {
-            for (int i = EndCol; i >= StartCol; i--)
+            for (int i = left; i <= right; i++)
             {
-                cout << a[EndRow][i] << " ";
+                cout << a[top][i] << " ";
             }
+            top++;
         }
-        EndRow--;
-
-        // Start Column
-        if (EndCol > StartCol)
+        else if (direction == 1)
         {
-            for (int i = EndRow; i >= StartRow; i--)
+            for (int i = top; i <= bottom; i++)
             {
-                cout << a[i][StartCol] << " ";
+                cout << a[i][right] << " ";
             }
+            right--;
         }
-        StartCol++;
+        else if (direction == 2)
+        {
+            for (int i = right; i >= left; i--)
+            {
+                cout << a[bottom][i] << " ";
+            }
+            bottom--;
+        }
+        else if (direction == 3)
+        {
+            for (int i = bottom; i >= top; i--)
+            {
+                cout << a[i][left] << " ";
+            }
+            left++;
+        }
+        direction = (direction + 1) % 4;
     }
 }
 
 int main()
 {
-    int m, n, a[20][20];
-    cout << "Enter number of rows and columns: ";
+    int a[20][20];
+    int m, n;
     cin >> m >> n;
 
-    cout << "Enter the elements of the matrix: " << endl;
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
@@ -57,16 +69,8 @@ int main()
             cin >> a[i][j];
         }
     }
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
     cout << endl;
-    cout << "Matrix after Spiral Print: " << endl;
+    cout << "The Spiral Print of matrix: ";
     Spiral(a, m, n);
     return 0;
 }
