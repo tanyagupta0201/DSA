13bd96104d4427105a8783ac6d7ea4c469e53e8a
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2021-07-24 16:57:18 +0530

    Update 03_Row_with_max_ones.cpp

--- Practice Problems/Matrix/03_Row_with_max_ones.cpp
+++ Practice Problems/Matrix/03_Row_with_max_ones.cpp
@@ -1,7 +1,16 @@
 /*  
-    
+      Given a boolean 2D array, where each row is sorted. Find the row with the maximum number of 1s. 
 
+      Example:  
 
+      Input matrix
+      0 1 1 1
+      0 0 1 1
+      1 1 1 1  // this row has maximum 1s
+      0 0 0 0
+
+      Output: 2
+  
 */
 
 #include <bits/stdc++.h>
