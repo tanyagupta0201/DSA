ff11f0d09b06cebc5978924a6d1ce5c14bbf7997
tanyakansal7 <you@example.com>
2021-10-09 10:26:19 +0530

    integer to roman

---
+++ String/IntegerToRoman.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/integer-to-roman/
+#include <bits/stdc++.h>
+class Solution
+{
+public:
+    string intToRoman(int num)
+    {
+        int intCode[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string code[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        string s;
+        for (int i = 0; i < 13; ++i)
+        {
+            while (num >= intCode[i])
+            {
+                s.append(code[i]);
+                num -= intCode[i];
+            }
+        }
+        return s;
+    }
+};
