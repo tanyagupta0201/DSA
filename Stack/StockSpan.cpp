57c8469f291c2be918793796b1c35dd168fc3ac8
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-12-16 08:45:00 +0530

    Update StockSpan.cpp

--- Stack/StockSpan.cpp
+++ Stack/StockSpan.cpp
@@ -1,3 +1,45 @@
+// TC = O(N), SC = O(N)
+
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       // Your code here
+       stack<pair<int, int>> st; // {Next greatest element. index}
+       
+       vector<int> v(n);
+    
+
+       for (int i = 0; i < n; i++)
+       {
+           if (st.size() > 0)
+           {
+               while (st.size() > 0 && st.top().first <= price[i])
+               {
+                  st.pop();
+               }
+           }
+
+           if (!st.empty())
+               v[i] = st.top().second;
+           else
+               v[i] = -1;
+
+           st.push({price[i], i});
+        }
+
+        for(int i = 0; i < v.size(); i++)
+        {
+            v[i] = i - v[i]; // curr index - next greatest element index
+        }
+       
+       return v;
+    }
+};
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
